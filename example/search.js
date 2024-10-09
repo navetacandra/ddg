5bbf3b2b686845f3655172c3d7e5342d01027c12
navetacandra <naveta.cand@gmail.com>
2024-10-09 19:42:17 +0700

    rollback: search example

--- example/search.js
+++ example/search.js
@@ -1,32 +1,32 @@
 const { search } = require("../src/index");
 
 (async () => {
-  // const regularSearch = await search({ query: "duckduckgo" } /*"regular"*/);
-  // console.log(regularSearch);
-  //
-  // if (regularSearch.hasNext) {
-  //   const nextRegularSearch = await search(
-  //     { query: "duckduckgo", next: regularSearch.next } /*"regular"*/,
-  //   );
-  //   console.log(nextRegularSearch);
-  // }
-  //
-  // const imageSearch = await search({ query: "duckduckgo" }, "image");
-  // console.log(imageSearch);
-  // if (imageSearch.hasNext) {
-  //   const imageRegularSearch = await search(
-  //     { query: "duckduckgo", next: imageSearch.next },
-  //     "image",
-  //   );
-  //   console.log(imageRegularSearch);
-  // }
-  //
-  // const videoSearch = await search({ query: "duckduckgo" }, "video");
-  // console.log(videoSearch);
+  const regularSearch = await search({ query: "duckduckgo" } /*"regular"*/);
+  console.log(regularSearch);
 
-  const newsSearch = await search({ query: "latest general news" }, "news");
+  if (regularSearch.hasNext) {
+    const nextRegularSearch = await search(
+      { query: "duckduckgo", next: regularSearch.next } /*"regular"*/,
+    );
+    console.log(nextRegularSearch);
+  }
+
+  const imageSearch = await search({ query: "duckduckgo" }, "image");
+  console.log(imageSearch);
+  if (imageSearch.hasNext) {
+    const imageRegularSearch = await search(
+      { query: "duckduckgo", next: imageSearch.next },
+      "image",
+    );
+    console.log(imageRegularSearch);
+  }
+
+  const videoSearch = await search({ query: "duckduckgo" }, "video");
+  console.log(videoSearch);
+
+  const newsSearch = await search({ query: "duckduckgo" }, "news");
   console.log(newsSearch);
 
-  // const locationSearch = await search({ query: "duckduckgo" }, "map");
-  // console.log(locationSearch);
+  const locationSearch = await search({ query: "duckduckgo" }, "map");
+  console.log(locationSearch);
 })();
